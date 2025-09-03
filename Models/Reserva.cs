bb8155dc8d35e8579436c416b64395a54a922382
Antonio <acao03@gmail.com>
2025-09-02 21:56:54 -0300

    Desafio entregue!

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -17,10 +17,7 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-
-                                 
+                                          
                 Hospedes = hospedes;
             
             if (Hospedes.Count != Suite.Capacidade)
@@ -39,22 +36,14 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
             return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
             decimal valor = 0;
 
 
-
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
             if (DiasReservados >= 10)
             {
                 valor = DiasReservados * (Suite.ValorDiaria - (Suite.ValorDiaria * 0.1M));
